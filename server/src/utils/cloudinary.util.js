155e3ee044dc56b35758b98bf29794e8e16cc6a0
vikas <vikasbhatt739@gmail.com>
2024-02-04 21:19:07 +0530

    new changes

--- server/src/utils/cloudinary.util.js
+++ server/src/utils/cloudinary.util.js
@@ -1,5 +1,10 @@
 import {v2 as cloudinary} from 'cloudinary'
 import fs from 'fs'
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './.env'
+})
 
 cloudinary.config({
   cloud_name: process.env.CLOUDNARY_CLOUD_NAME,
